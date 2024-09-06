826fe3ffc488c85993a601a3a34caa1f08360472
Gavin P <gavinjpeeler@gmail.com>
2024-09-05 23:48:48 -0700

    added git ignore

--- first-program/Main.java
+++ first-program/Main.java
@@ -1,8 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         
         // Don't worry about the stuff on the outside right now.
         // Just type in here!
-        
     }
 }
