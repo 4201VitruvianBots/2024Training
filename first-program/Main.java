c684157816496795a73cac5f7db8d077cc1e45ad
Gavin P <gavinjpeeler@gmail.com>
2024-09-06 15:57:40 -0700

    cleanup

--- first-program/Main.java
+++ first-program/Main.java
@@ -4,16 +4,5 @@ public class Main {
         // Don't worry about the stuff on the outside right now.
         // Just type in here!
         
-        int a = 5;
-        int b = 10;
-        int c = 15;
-        
-        if (b > a && c > b) {
-            System.out.println("Both conditions are true!");
-        }
-        
-        if (b > a || c > b) {
-            System.out.println("At least one condition is true!");
-        }
     }
 }
